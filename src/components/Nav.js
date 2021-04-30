a45e518945553054e37237a2507bdf3e91f94703
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-30 22:51:37 +0400

    added Nav component for rendering navs, via mapping nav arrays

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+const Nav = (props) => {
+  return (
+    <nav className={`${props.className}__nav`}>
+      <ul className={`${props.className}__list`}>
+        {props.arr.map((item) => (
+          <li className={`${props.className}__list-item`} key={item.id}>
+            <a href={item.link}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
