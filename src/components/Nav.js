10d2927997b5c5b1c8436a0bba9cc74fd0c8d35e
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-02 01:27:52 +0400

    added classname to the nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,9 @@ const Nav = (props) => {
       <ul className={`${props.className}__list`}>
         {props.arr.map((item) => (
           <li className={`${props.className}__list-item`} key={item.id}>
-            <a href={item.link}>{item.name}</a>
+            <a href={item.link} className={`${props.className}__link`}>
+              {item.name}
+            </a>
           </li>
         ))}
       </ul>
