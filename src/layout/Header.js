187287724902789eb7f0cefa7c74ebf8d8444f59
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-02 15:53:14 +0400

    header desktop version done

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,40 +5,38 @@ import Heading from "../components/Heading";
 import Button from "../components/Button";
 
 const navArr = [
-  { name: "მთავარი", link: "#link-1", id: "1" },
-  { name: "ჩვენს შესახებ", link: "#link-2", id: "2" },
-  { name: "სერვისები", link: "#link-3", id: "3" },
-  { name: "კონტაქტი", link: "#link-4", id: "4" },
+  { name: "მთავარი", link: "#main", id: "1" },
+  { name: "ჩვენს შესახებ", link: "#about", id: "2" },
+  { name: "სერვისები", link: "#services", id: "3" },
+  { name: "კონტაქტი", link: "#contact", id: "4" },
 ];
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="overlay">
-        <div className="container">
-          <div className="header__top">
-            <div className="header__logo-container">
-              <a href="https://www.kokhtad.ge" className="header__logo-link">
-                <Image src={srcLogo} alt="Logo" className="header__logo" />
-              </a>
-            </div>
-            <Nav arr={navArr} className="header" />
+    <header className="header" id="main">
+      <div className="container">
+        <div className="header__top">
+          <div className="header__logo-container">
+            <a href="https://www.kokhtad.ge" className="header__logo-link">
+              <Image src={srcLogo} alt="Logo" className="header__logo" />
+            </a>
           </div>
-          <div className="header__bottom">
-            <div className="header__heading-container">
-              <Heading
-                element="h1"
-                className="heading-primary"
-                text="კოხტად.ჯი - შენი კომფორტის სერვისი"
-              />
-            </div>
-            <div className="header__button-container">
-              <Button
-                className="btn-primary"
-                content="გაიგე მეტი"
-                href="#link-2"
-              />
-            </div>
+          <Nav arr={navArr} className="header" />
+        </div>
+        <div className="header__bottom">
+          <div className="header__heading-container">
+            <Heading
+              element="h1"
+              className="heading-primary"
+              text="კოხტად.ჯი - შენი კომფორტის სერვისი"
+            />
+          </div>
+          <div className="header__button-container">
+            <Button
+              className="btn-primary"
+              content="გაიგე მეტი"
+              href="#link-2"
+            />
           </div>
         </div>
       </div>
