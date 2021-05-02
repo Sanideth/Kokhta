0b37c044069d1afbc520906021ecd8b50217898e
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-02 15:54:36 +0400

    desktop styling pretty much done

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,15 +1,16 @@
 import Image from "./Image";
 import Button from "./Button";
+
 const Service = (props) => {
   const renderButton = props.renderButton;
   return (
-    <figure className={`${props.className}service`}>
+    <figure className={`${props.className}service`} onClick={props.onClick}>
       {renderButton ? (
         <div className={`${props.className}__button-container`}>
           <Button content={props.content} className={props.buttonClassName} />
         </div>
       ) : null}
-      <Image className={props.className} src={props.src} alt={props.alt} />
+      <Image className={props.imageClassName} src={props.src} alt={props.alt} />
       <figcaption className={`${props.className}figcaption`}>
         {props.text}
       </figcaption>
