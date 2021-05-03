98b7eb13b5f7e4072189d326ee6f38de398832c2
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-03 16:15:03 +0400

    added some props to the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,19 @@
 const Button = (props) => {
+  const clickFunctions = (popupText, popupHeading) => {
+    props.clickOpen();
+    props.clickText(popupText, popupHeading);
+  };
+  const clickable = props.clickable;
   return (
-    <a className={`btn ${props.className}`} href={props.href}>
+    <a
+      className={`btn ${props.className}`}
+      href={props.href}
+      onClick={
+        clickable
+          ? () => clickFunctions(props.popupText, props.popupHeading)
+          : null
+      }
+    >
       {props.content}
     </a>
   );
