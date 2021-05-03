1de29b3a1b17d632eb8ca7d555b61af238b39489
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-03 16:14:29 +0400

    added popup component

---
+++ src/layout/Popup.js
@@ -0,0 +1,26 @@
+import Heading from "../components/Heading";
+import Icon from "../components/Icon";
+import { ReactComponent as CloseIcon } from "../img/close.svg";
+const Popup = (props) => {
+  return (
+    <div className="popup__content">
+      <div className="popup__button-container">
+        <button className="btn btn-tertiary" onClick={props.click}>
+          <Icon icon={<CloseIcon />} />
+        </button>
+      </div>
+      <div className="popup__text-container">
+        <div className="popup__heading-container">
+          <Heading
+            element="h3"
+            className="heading-tertiary"
+            text={props.popupHeading}
+          />
+        </div>
+        {props.popupText}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
