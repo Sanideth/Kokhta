ed72452b10ab404e1a04e230714ce04a1a06fefd
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-04 16:23:13 +0400

    added some conditionally rendered classes and onclick function to links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
 const Nav = (props) => {
   return (
-    <nav className={`${props.className}__nav`}>
+    <nav
+      className={`${props.className}__nav ${
+        props.navActive ? "nav-active" : ""
+      }`}
+    >
       <ul className={`${props.className}__list`}>
         {props.arr.map((item) => (
-          <li className={`${props.className}__list-item`} key={item.id}>
-            <a href={item.link} className={`${props.className}__link`}>
+          <li
+            className={`${props.className}__list-item ${
+              props.navActive ? "list-item-active" : ""
+            }`}
+            key={item.id}
+          >
+            <a
+              href={item.link}
+              className={`${props.className}__link`}
+              onClick={props.onClick}
+            >
               {item.name}
             </a>
           </li>
