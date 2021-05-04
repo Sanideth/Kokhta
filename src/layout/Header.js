5238a25f37082a2d66f61f553b86adb16f256e06
Sandro Bochorishvili <Sanideth@gmail.com>
2021-05-04 16:24:31 +0400

    added state to control mobile navigation and corresponding style changes

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,6 +3,7 @@ import srcLogo from "../img/logo.png";
 import Nav from "../components/Nav";
 import Heading from "../components/Heading";
 import Button from "../components/Button";
+import { useState } from "react";
 
 const navArr = [
   { name: "მთავარი", link: "#main", id: "1" },
@@ -12,16 +13,59 @@ const navArr = [
 ];
 
 const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+  console.log(navActive);
+  const handleOnClick = () => {
+    setNavActive(!navActive);
+  };
   return (
-    <header className="header" id="main">
-      <div className="container">
+    <header
+      className={`header ${navActive ? "header-active" : "null"}`}
+      id="main"
+    >
+      <div className="container u-fl">
         <div className="header__top">
           <div className="header__logo-container">
             <a href="https://www.kokhtad.ge" className="header__logo-link">
               <Image src={srcLogo} alt="Logo" className="header__logo" />
             </a>
           </div>
-          <Nav arr={navArr} className="header" />
+          <Nav
+            arr={navArr}
+            className="header"
+            navActive={navActive}
+            onClick={handleOnClick}
+          />
+          <div className="header__nav-button-container">
+            <button
+              className={`btn btn-navigation ${
+                navActive ? "btn-navigation-active" : ""
+              }`}
+              onClick={handleOnClick}
+            >
+              <span
+                className={`btn-span btn-span--1 ${
+                  navActive ? "span1-active" : ""
+                }`}
+              >
+                &nbsp;
+              </span>
+              <span
+                className={`btn-span btn-span--2 ${
+                  navActive ? "span2-deactive" : ""
+                }`}
+              >
+                &nbsp;
+              </span>
+              <span
+                className={`btn-span btn-span--3 ${
+                  navActive ? "span3-active" : ""
+                }`}
+              >
+                &nbsp;
+              </span>
+            </button>
+          </div>
         </div>
         <div className="header__bottom">
           <div className="header__heading-container">
