b4448b578d879fcef32d81d3bfd6ac8826f09159
S.Botchorishvili <S.Botchorishvili@tbcpay.ge>
2024-11-03 03:12:00 +0400

    removed obsolete preloader

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Ripple } from 'react-preloaders2';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Ripple color="#fff" background="#449440" />
   </React.StrictMode>,
   document.getElementById('root')
 );
